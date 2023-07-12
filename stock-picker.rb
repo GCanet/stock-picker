837c13445b6949fa3711e50db4bddb804a2e9e81
Gerard <kanetpwnz@gmail.com>
2023-07-12 23:26:53 +0200

    initial commit

---
+++ stock-picker.rb
@@ -0,0 +1,14 @@
+0. crear array resultado vacia
+1. buscar numero mas bajo en la array
+2. en que value esta? 0,1,2,3,4 -> pasar como primer valor a la array
+3. buscar numero mas alto en la array
+4. en que value esta? 0,1,2,3,4 -> pasar como segundo valor a la array
+5. retornar la nueva array
+
+dayPRICES = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(Hash)
+  resultado = Hash.new
+
+  return resultado
+end
